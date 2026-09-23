d72bd3fe771672073199e13d0dab49b18f89deda
nikhilkush2425-pixel <nikhilkush2425@gmail.com>
2026-09-24 01:28:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,9 +43,16 @@ app.use(express.json());
 // MySQL connection
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 2,
+    connectTimeout: 30000,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Test route
